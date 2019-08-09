ffbdb64f4d45a5ed57367ce1d8996fbbb18375a9
sleepey bunney <obsoletist@gmail.com>
2019-08-09 02:58:22 +0100

    write field scripts

--- FF8Mod/FieldScript.cs
+++ FF8Mod/FieldScript.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace FF8Mod
 {
     public partial class FieldScript
     {
         public List<Entity> Entities;
-
+        
+        // construct from binary data (ie. game files)
         public FieldScript(byte[] data)
         {
             using (var stream = new MemoryStream(data))
@@ -19,50 +21,43 @@ namespace FF8Mod
                 var otherCount = reader.ReadByte();
                 var totalCount = doorCount + lineCount + backgroundCount + otherCount;
 
-                var firstSectionOffset = reader.ReadUInt16();
+                var scriptInfoOffset = reader.ReadUInt16();
                 var scriptDataOffset = reader.ReadUInt16();
 
+                // script counts & labels for each entity
                 var entityInfo = new List<EntityInfo>();
                 for (var i = 0; i < totalCount; i++)
                 {
+                    // entity types are inferred from their order in the file
                     var type = EntityType.Line;
                     if (i >= lineCount) type = EntityType.Door;
                     if (i >= lineCount + doorCount) type = EntityType.Background;
                     if (i >= lineCount + doorCount + backgroundCount) type = EntityType.Other;
 
-                    var value = reader.ReadUInt16();
-                    entityInfo.Add(new EntityInfo(type, value & 0x7f, value >> 7));
+                    entityInfo.Add(new EntityInfo(type, reader.ReadUInt16()));
                 }
 
+                // script offsets within the data block
                 var scriptInfo = new List<ScriptInfo>();
-                var scriptCount = (scriptDataOffset - firstSectionOffset) / 2;
+                var scriptCount = (scriptDataOffset - scriptInfoOffset) / 2;
                 for (var i = 0; i < scriptCount; i++)
                 {
-                    var value = reader.ReadUInt16();
-                    scriptInfo.Add(new ScriptInfo((value & 0x7fff) * 4, value >> 15));
+                    scriptInfo.Add(new ScriptInfo(reader.ReadUInt16()));
                 }
 
+                // script data
                 var scripts = new List<List<Instruction>>();
                 for (int i = 0; i < scriptCount - 1; i++)
                 {
                     var script = new List<Instruction>();
                     while (stream.Position < scriptDataOffset + scriptInfo[i + 1].Position)
                     {
-                        var value = BitConverter.ToInt32(reader.ReadBytes(4), 0);
-                        var hasParam = (value & 0xff000000) != 0;
-                        if (hasParam)
-                        {
-                            var param = unchecked((short)(value & 0x00ffffff));
-                            script.Add(new Instruction(value >> 24, param));
-                        }
-                        else
-                        {
-                            script.Add(new Instruction(value));
-                        }
+                        script.Add(new Instruction(reader.ReadBytes(4)));
                     }
                     scripts.Add(script);
                 }
 
+                // put it all together
                 Entities = new List<Entity>();
                 for (int i = 0; i < entityInfo.Count; i++)
                 {
@@ -78,6 +73,93 @@ namespace FF8Mod
             }
         }
 
+        // jsm output
+        public byte[] Encoded
+        {
+            get
+            {
+                var doorCount = 0;
+                var lineCount = 0;
+                var backgroundCount = 0;
+                var otherCount = 0;
+                var allScripts = new List<Script>();
+
+                foreach (var e in Entities)
+                {
+                    switch (e.Type)
+                    {
+                        case EntityType.Door:
+                            doorCount++;
+                            break;
+                        case EntityType.Line:
+                            lineCount++;
+                            break;
+                        case EntityType.Background:
+                            backgroundCount++;
+                            break;
+                        default:
+                            otherCount++;
+                            break;
+                    }
+
+                    allScripts.AddRange(e.Scripts);
+                }
+
+                var entityInfoLength = Entities.Count * 2;                              // 2 bytes per entity
+                var scriptInfoLength = allScripts.Count * 2 + 2;                        // 2 bytes per script + an extra entry to denote EOF
+                var scriptDataLength = allScripts.Sum(s => s.Instructions.Count * 4);   // 4 bytes per instruction
+                var totalLength = 8 + entityInfoLength + scriptInfoLength + scriptDataLength;
+
+                var scriptInfoOffset = entityInfoLength + 8;                            // entity counts above + these two offsets = 8 bytes
+                var scriptDataOffset = scriptInfoLength + scriptInfoOffset;
+
+                var result = new byte[totalLength];
+
+                using (var stream = new MemoryStream(result))
+                using (var writer = new BinaryWriter(stream))
+                {
+                    writer.Write((byte)doorCount);
+                    writer.Write((byte)lineCount);
+                    writer.Write((byte)backgroundCount);
+                    writer.Write((byte)otherCount);
+                    writer.Write((short)scriptInfoOffset);
+                    writer.Write((short)scriptDataOffset);
+
+                    // entity info block
+                    // todo: make sure everything's still in the right order (entity types -> script labels)
+                    foreach (var e in Entities)
+                    {
+                        writer.Write(new EntityInfo(e).Encoded);
+                    }
+
+                    // script info block (& gathering script data while we're here)
+                    var scriptData = new List<int>();
+                    foreach (var s in allScripts)
+                    {
+                        var info = new ScriptInfo(scriptData.Count * 4, s.MysteryFlag ? 1 : 0);
+                        writer.Write(info.Encoded);
+
+                        foreach (var i in s.Instructions)
+                        {
+                            scriptData.Add(i.Encoded);
+                        }
+                    }
+
+                    // extra eof entry
+                    writer.Write(new ScriptInfo(scriptData.Count * 4, 0).Encoded);
+
+                    // script data block
+                    foreach (var s in scriptData)
+                    {
+                        writer.Write(s);
+                    }
+                }
+
+                return result;
+            }
+        }
+
+        // field entity (eg. a person or an event trigger)
         public class Entity
         {
             public EntityType Type;
@@ -88,10 +170,9 @@ namespace FF8Mod
                 Type = type;
                 Scripts = scripts;
             }
-
-            public Entity(EntityType type) : this(type, new List<Script>()) { }
         }
 
+        // code attached to an entity
         public class Script
         {
             public List<Instruction> Instructions;
@@ -104,7 +185,8 @@ namespace FF8Mod
             }
         }
 
-        public struct EntityInfo
+        // header data for entities in the jsm file
+        public class EntityInfo
         {
             public EntityType Type;
             public int ScriptCount;
@@ -116,9 +198,29 @@ namespace FF8Mod
                 ScriptCount = scriptCount;
                 Label = label;
             }
+
+            public EntityInfo(EntityType type, ushort data)
+            {
+                Type = type;
+                ScriptCount = data & 0x7f;
+                Label = data >> 7;
+            }
+
+            public EntityInfo(Entity entity)
+            {
+                Type = entity.Type;
+                ScriptCount = entity.Scripts.Count - 1;     // entry script (the one referred to by Label) isn't counted
+                Label = entity.Scripts[0].Instructions[0].Param;
+            }
+
+            public ushort Encoded
+            {
+                get { return (ushort)((Label << 7) + ScriptCount); }
+            }
         }
 
-        public struct ScriptInfo
+        // header data for scripts in the jsm file
+        public class ScriptInfo
         {
             public int Position;
             public int Flag;
@@ -128,9 +230,20 @@ namespace FF8Mod
                 Position = position;
                 Flag = flag;
             }
+
+            public ScriptInfo(ushort data)
+            {
+                Position = (data & 0x7fff) * 4;
+                Flag = data >> 15;
+            }
+
+            public ushort Encoded
+            {
+                get { return (ushort)((Flag << 15) + Position / 4); }
+            }
         }
 
-        public struct Instruction
+        public class Instruction
         {
             public int OpCode;
             public int Param;
@@ -148,6 +261,32 @@ namespace FF8Mod
                 OpCode = opCode;
                 Param = 0;
                 HasParam = false;
+            }
+
+            public Instruction(byte[] instruction)
+            {
+                var value = BitConverter.ToInt32(instruction, 0);
+                HasParam = (value & 0xff000000) != 0;
+
+                if (HasParam)
+                {
+                    OpCode = value >> 24;
+                    Param = unchecked((short)(value & 0x00ffffff));
+                }
+                else
+                {
+                    OpCode = value;
+                    Param = 0;
+                }
+            }
+
+            public int Encoded
+            {
+                get
+                {
+                    if (HasParam) return (OpCode << 24) | (Param & 0x00ffffff);
+                    else return OpCode;
+                }
             }
 
             public override string ToString()
