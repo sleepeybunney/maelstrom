aacdf5e0361c40f89e53e5c9b90eb836e4d65253
sleepey bunney <obsoletist@gmail.com>
2019-08-10 01:51:25 +0100

    add unit tests for battle scripts and monsters

---
+++ FF8ModTest/BattleScriptTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FF8Mod;
+using Xunit;
+
+namespace FF8ModTest
+{
+    public class BattleScriptTest
+    {
+        [Fact]
+        public void EmptyScriptTest()
+        {
+            var script = new BattleScript();
+
+            // empty sub-scripts should each contain exactly 1 instruction (return)
+            Assert.NotNull(script.Init);
+            Assert.NotEmpty(script.Init);
+            Assert.Equal(0, script.Init[0].Op.Code);
+
+            Assert.NotNull(script.Execute);
+            Assert.NotEmpty(script.Execute);
+            Assert.Equal(0, script.Execute[0].Op.Code);
+
+            Assert.NotNull(script.Counter);
+            Assert.NotEmpty(script.Counter);
+            Assert.Equal(0, script.Counter[0].Op.Code);
+
+            Assert.NotNull(script.Death);
+            Assert.NotEmpty(script.Death);
+            Assert.Equal(0, script.Death[0].Op.Code);
+
+            Assert.NotNull(script.PreCounter);
+            Assert.NotEmpty(script.PreCounter);
+            Assert.Equal(0, script.PreCounter[0].Op.Code);
+        }
+
+        [Theory]
+        [InlineData(0x08, new short[] { })]
+        [InlineData(0x01, new short[] { 5 })]
+        [InlineData(0x02, new short[] { 3, 200, 0, 76, 11 })]
+        [InlineData(0x0b, new short[] { 0, 2, 3 })]
+        [InlineData(0x23, new short[] { -285 })]
+        public void EncodingTest(byte opcode, short[] args)
+        {
+            // construct a script with a populated init sub-script & run it through the encoder
+            var script = new BattleScript();
+            script.Init.Insert(0, new BattleScript.Instruction(BattleScript.OpCodes[opcode], args));
+            script = new BattleScript(script.Encoded);
+
+            // added instruction should still be intact
+            Assert.NotNull(script.Init);
+            Assert.NotEmpty(script.Init);
+            Assert.Equal(opcode, script.Init[0].Op.Code);
+            Assert.Equal(args.Length, script.Init[0].Args.Length);
+            for (int i = 0; i < args.Length; i++)
+            {
+                Assert.Equal(args[i], script.Init[0].Args[i]);
+            }
+            Assert.Equal(0, script.Init[1].Op.Code);
+
+            // everything else should be the same as the empty script
+            Assert.NotNull(script.Execute);
+            Assert.NotEmpty(script.Execute);
+            Assert.Equal(0, script.Execute[0].Op.Code);
+
+            Assert.NotNull(script.Counter);
+            Assert.NotEmpty(script.Counter);
+            Assert.Equal(0, script.Counter[0].Op.Code);
+
+            Assert.NotNull(script.Death);
+            Assert.NotEmpty(script.Death);
+            Assert.Equal(0, script.Death[0].Op.Code);
+
+            Assert.NotNull(script.PreCounter);
+            Assert.NotEmpty(script.PreCounter);
+            Assert.Equal(0, script.PreCounter[0].Op.Code);
+        }
+    }
+}
---
+++ FF8ModTest/MonsterTest.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FF8Mod;
+using Xunit;
+
+namespace FF8ModTest
+{
+    public class MonsterTest
+    {
+        [Fact]
+        public void FullTest()
+        {
+            // load a real monster dat file from the game
+            var minotaur = Monster.FromFile(@"TestData\c0m062.dat");
+
+            // check various known values
+            Assert.Equal("Minotaur", minotaur.Info.Name);
+
+            Assert.Equal(100, minotaur.Info.Hp[0]);
+            Assert.Equal(75, minotaur.Info.Hp[1]);
+            Assert.Equal(0, minotaur.Info.Hp[2]);
+            Assert.Equal(0, minotaur.Info.Hp[3]);
+
+            Assert.Equal(100, minotaur.Info.Str[0]);
+            Assert.Equal(5, minotaur.Info.Str[1]);
+            Assert.Equal(40, minotaur.Info.Str[2]);
+            Assert.Equal(100, minotaur.Info.Str[3]);
+
+            Assert.Equal(48, minotaur.Info.Mag[0]);
+            Assert.Equal(2, minotaur.Info.Mag[1]);
+            Assert.Equal(140, minotaur.Info.Mag[2]);
+            Assert.Equal(160, minotaur.Info.Mag[3]);
+
+            Assert.Equal(1, minotaur.Info.Vit[0]);
+            Assert.Equal(8, minotaur.Info.Vit[1]);
+            Assert.Equal(60, minotaur.Info.Vit[2]);
+            Assert.Equal(1, minotaur.Info.Vit[3]);
+
+            Assert.Equal(2, minotaur.Info.Spr[0]);
+            Assert.Equal(20, minotaur.Info.Spr[1]);
+            Assert.Equal(30, minotaur.Info.Spr[2]);
+            Assert.Equal(1, minotaur.Info.Spr[3]);
+
+            Assert.Equal(0, minotaur.Info.Spd[0]);
+            Assert.Equal(2, minotaur.Info.Spd[1]);
+            Assert.Equal(10, minotaur.Info.Spd[2]);
+            Assert.Equal(16, minotaur.Info.Spd[3]);
+
+            Assert.Equal(0, minotaur.Info.Eva[0]);
+            Assert.Equal(8, minotaur.Info.Eva[1]);
+            Assert.Equal(0, minotaur.Info.Eva[2]);
+            Assert.Equal(24, minotaur.Info.Eva[3]);
+
+            Assert.Equal(31455, minotaur.Info.HpAtLevel(69));
+            Assert.Equal(92, minotaur.Info.StrAtLevel(33));
+            Assert.Equal(42, minotaur.Info.MagAtLevel(6));
+            Assert.Equal(65, minotaur.Info.VitAtLevel(40));
+            Assert.Equal(75, minotaur.Info.SprAtLevel(43));
+            Assert.Equal(50, minotaur.Info.SpdAtLevel(90));
+            Assert.Equal(8, minotaur.Info.EvaAtLevel(100));
+
+            Assert.Equal(2u, minotaur.Info.AbilitiesLow[0].AbilityId);
+            Assert.Equal(213u, minotaur.Info.AbilitiesMed[2].AbilityId);
+            Assert.Equal(220u, minotaur.Info.AbilitiesHigh[3].AbilityId);
+
+            Assert.Equal(0x00, minotaur.AI.Scripts.Init[0].Op.Code);
+            Assert.Equal(0x00, minotaur.AI.Scripts.PreCounter[0].Op.Code);
+            Assert.Equal(22, minotaur.AI.Scripts.Execute.Count);
+            Assert.Equal(0x0c, minotaur.AI.Scripts.Execute[20].Op.Code);
+            Assert.Equal(0, minotaur.AI.Scripts.Execute[20].Args[0]);
+            Assert.Equal(4, minotaur.AI.Scripts.Counter.Count);
+            Assert.Equal(0x23, minotaur.AI.Scripts.Counter[1].Op.Code);
+            Assert.Equal(3, minotaur.AI.Scripts.Counter[1].Args[0]);
+            Assert.Equal(11, minotaur.AI.Scripts.Death.Count);
+
+            Assert.Equal(4, minotaur.AI.Strings.Count);
+            Assert.Equal("Minotaur “may we join you?”", minotaur.AI.Strings[3]);
+
+            // run it through the encoder & check everything again
+            minotaur = Monster.FromBytes(minotaur.Encoded);
+            Assert.Equal("Minotaur", minotaur.Info.Name);
+
+            Assert.Equal(100, minotaur.Info.Hp[0]);
+            Assert.Equal(75, minotaur.Info.Hp[1]);
+            Assert.Equal(0, minotaur.Info.Hp[2]);
+            Assert.Equal(0, minotaur.Info.Hp[3]);
+
+            Assert.Equal(100, minotaur.Info.Str[0]);
+            Assert.Equal(5, minotaur.Info.Str[1]);
+            Assert.Equal(40, minotaur.Info.Str[2]);
+            Assert.Equal(100, minotaur.Info.Str[3]);
+
+            Assert.Equal(48, minotaur.Info.Mag[0]);
+            Assert.Equal(2, minotaur.Info.Mag[1]);
+            Assert.Equal(140, minotaur.Info.Mag[2]);
+            Assert.Equal(160, minotaur.Info.Mag[3]);
+
+            Assert.Equal(1, minotaur.Info.Vit[0]);
+            Assert.Equal(8, minotaur.Info.Vit[1]);
+            Assert.Equal(60, minotaur.Info.Vit[2]);
+            Assert.Equal(1, minotaur.Info.Vit[3]);
+
+            Assert.Equal(2, minotaur.Info.Spr[0]);
+            Assert.Equal(20, minotaur.Info.Spr[1]);
+            Assert.Equal(30, minotaur.Info.Spr[2]);
+            Assert.Equal(1, minotaur.Info.Spr[3]);
+
+            Assert.Equal(0, minotaur.Info.Spd[0]);
+            Assert.Equal(2, minotaur.Info.Spd[1]);
+            Assert.Equal(10, minotaur.Info.Spd[2]);
+            Assert.Equal(16, minotaur.Info.Spd[3]);
+
+            Assert.Equal(0, minotaur.Info.Eva[0]);
+            Assert.Equal(8, minotaur.Info.Eva[1]);
+            Assert.Equal(0, minotaur.Info.Eva[2]);
+            Assert.Equal(24, minotaur.Info.Eva[3]);
+
+            Assert.Equal(31455, minotaur.Info.HpAtLevel(69));
+            Assert.Equal(92, minotaur.Info.StrAtLevel(33));
+            Assert.Equal(42, minotaur.Info.MagAtLevel(6));
+            Assert.Equal(65, minotaur.Info.VitAtLevel(40));
+            Assert.Equal(75, minotaur.Info.SprAtLevel(43));
+            Assert.Equal(50, minotaur.Info.SpdAtLevel(90));
+            Assert.Equal(8, minotaur.Info.EvaAtLevel(100));
+
+            Assert.Equal(2u, minotaur.Info.AbilitiesLow[0].AbilityId);
+            Assert.Equal(213u, minotaur.Info.AbilitiesMed[2].AbilityId);
+            Assert.Equal(220u, minotaur.Info.AbilitiesHigh[3].AbilityId);
+
+            Assert.Equal(0x00, minotaur.AI.Scripts.Init[0].Op.Code);
+            Assert.Equal(0x00, minotaur.AI.Scripts.PreCounter[0].Op.Code);
+            Assert.Equal(22, minotaur.AI.Scripts.Execute.Count);
+            Assert.Equal(0x0c, minotaur.AI.Scripts.Execute[20].Op.Code);
+            Assert.Equal(0, minotaur.AI.Scripts.Execute[20].Args[0]);
+            Assert.Equal(4, minotaur.AI.Scripts.Counter.Count);
+            Assert.Equal(0x23, minotaur.AI.Scripts.Counter[1].Op.Code);
+            Assert.Equal(3, minotaur.AI.Scripts.Counter[1].Args[0]);
+            Assert.Equal(11, minotaur.AI.Scripts.Death.Count);
+
+            Assert.Equal(4, minotaur.AI.Strings.Count);
+            Assert.Equal("Minotaur “may we join you?”", minotaur.AI.Strings[3]);
+        }
+    }
+}
