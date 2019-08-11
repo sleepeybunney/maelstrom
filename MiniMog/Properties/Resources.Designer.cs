0aac76fc7a00529a9c3f8eb76febd9f1299073c9
sleepey bunney <obsoletist@gmail.com>
2019-08-11 05:27:08 +0100

    update root namespace in visual studio

--- MiniMog/Properties/Resources.Designer.cs
+++ MiniMog/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace FF8Mod.MiniMog.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MiniMog.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FF8Mod.MiniMog.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
