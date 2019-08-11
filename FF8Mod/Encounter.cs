62ce2708523009b300d6c528cfe350305db01d89
sleepey bunney <obsoletist@gmail.com>
2019-08-11 04:52:23 +0100

    read encounter data

---
+++ FF8Mod/Encounter.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FF8Mod
+{
+    public class Encounter
+    {
+        public byte Scene;
+        public bool NoEscape, NoVictorySequence, ShowTimer, NoExp, NoResults, StruckFirst, BackAttack, ScriptedBattle;
+        public byte MainCamera, MainCameraAnimation, SecondaryCamera, SecondaryCameraAnimation;
+        public EncounterSlot[] Slots;
+
+        public Encounter()
+        {
+            Scene = 0;
+            NoEscape = false;
+            NoVictorySequence = false;
+            ShowTimer = false;
+            NoExp = false;
+            NoResults = false;
+            StruckFirst = false;
+            BackAttack = false;
+            ScriptedBattle = false;
+            MainCamera = 0;
+            MainCameraAnimation = 0;
+            SecondaryCamera = 0;
+            SecondaryCameraAnimation = 0;
+            Slots = new EncounterSlot[8];
+            for (int i = 0; i < 8; i++) Slots[i] = new EncounterSlot();
+        }
+
+        public static Encounter FromBytes(int id, byte[] data)
+        {
+            var result = new Encounter();
+
+            using (var stream = new MemoryStream(data))
+            using (var reader = new BinaryReader(stream))
+            {
+                stream.Seek(id * 128, SeekOrigin.Begin);
+                result.Scene = reader.ReadByte();
+
+                var flags = reader.ReadByte();
+                result.NoEscape = (flags & 1) != 0;
+                result.NoVictorySequence = (flags & 2) != 0;
+                result.ShowTimer = (flags & 4) != 0;
+                result.NoExp = (flags & 8) != 0;
+                result.NoResults = (flags & 16) != 0;
+                result.StruckFirst = (flags & 32) != 0;
+                result.BackAttack = (flags & 64) != 0;
+                result.ScriptedBattle = (flags & 128) != 0;
+
+                var mainCamera = reader.ReadByte();
+                result.MainCamera = (byte)(mainCamera >> 4);
+                result.MainCameraAnimation = (byte)(mainCamera & 0x0F);
+
+                var secondaryCamera = reader.ReadByte();
+                result.SecondaryCamera = (byte)(secondaryCamera >> 4);
+                result.SecondaryCameraAnimation = (byte)(secondaryCamera & 0x0F);
+
+                var hidden = reader.ReadByte();
+                var unloaded = reader.ReadByte();
+                var untargetable = reader.ReadByte();
+                var enabled = reader.ReadByte();
+
+                for (int i = 0; i < 8; i++)
+                {
+                    result.Slots[i].Hidden = ((hidden >> (7 - i)) & 1) != 0;
+                    result.Slots[i].Unloaded = ((unloaded >> (7 - i)) & 1) != 0;
+                    result.Slots[i].Untargetable = ((untargetable >> (7 - i)) & 1) != 0;
+                    result.Slots[i].Enabled = ((enabled >> (7 - i)) & 1) != 0;
+                    result.Slots[i].Position = new EncounterSlot.Coords(reader.ReadInt16(), reader.ReadInt16(), reader.ReadInt16());
+                }
+
+                foreach (var s in result.Slots) s.MonsterID = reader.ReadByte();
+                foreach (var s in result.Slots) s.Unknown1 = reader.ReadInt16();
+                foreach (var s in result.Slots) s.Unknown2 = reader.ReadInt16();
+                foreach (var s in result.Slots) s.Unknown3 = reader.ReadInt16();
+                foreach (var s in result.Slots) s.Unknown4 = reader.ReadByte();
+                foreach (var s in result.Slots) s.Level = reader.ReadByte();
+            }
+
+            return result;
+        }
+
+        public static Encounter FromFile(int id, string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException("Encounter file not found");
+            }
+
+            return FromBytes(id, File.ReadAllBytes(path));
+        }
+
+        public static Encounter FromSource(int id, FileSource source, string path)
+        {
+            return FromBytes(id, source.GetFile(path));
+        }
+    }
+}
---
+++ FF8Mod/EncounterSlot.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FF8Mod
+{
+    public class EncounterSlot
+    {
+        public byte MonsterID;
+        public Coords Position;
+        public byte Level;
+        public bool Enabled, Unloaded, Untargetable, Hidden;
+        public short Unknown1, Unknown2, Unknown3;
+        public byte Unknown4;
+
+        public EncounterSlot()
+        {
+            MonsterID = 0x10;
+            Level = 0xff;
+            Position = new Coords(0, 0, -5700);
+            Enabled = false;
+            Unloaded = false;
+            Untargetable = false;
+            Hidden = false;
+        }
+
+        public Monster GetMonster(FileSource battleSource)
+        {
+            return Monster.FromSource(battleSource, string.Format(@"c:\ff8\data\eng\battle\c0m{0:d3}.dat", MonsterID - 0x10));
+        }
+
+        public class Coords
+        {
+            public short X, Y, Z;
+
+            public Coords(short x, short y, short z)
+            {
+                X = x;
+                Y = y;
+                Z = z;
+            }
+        }
+    }
+}
