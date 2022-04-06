f630c233e5e2a05c1679bd0315e4ba0a4e0f6bca
Sleepey Bunney <obsoletist@gmail.com>
2022-04-06 20:44:33 +0100

    add tests for IndexEntry

--- FF8Mod/Archive/IndexEntry.cs
+++ FF8Mod/Archive/IndexEntry.cs
@@ -13,11 +13,13 @@ namespace Sleepey.FF8Mod.Archive
 
         public IndexEntry(IEnumerable<byte> data)
         {
+            if (data == null) throw new ArgumentNullException("data");
             if (data.Count() != 12) throw new InvalidDataException("Expected 12 bytes for a file index entry");
             var dataArray = data.ToArray();
             Length = BitConverter.ToUInt32(dataArray, 0);
             Location = BitConverter.ToUInt32(dataArray, 4);
             Compression = BitConverter.ToUInt32(dataArray, 8);
+            if (Compression > 2) throw new InvalidDataException("Unrecognised compression format: " + Compression.ToString());
         }
 
         public IndexEntry(uint location, uint length, uint compression)
@@ -25,6 +27,7 @@ namespace Sleepey.FF8Mod.Archive
             Location = location;
             Length = length;
             Compression = compression;
+            if (Compression > 2) throw new InvalidDataException("Unrecognised compression format: " + Compression.ToString());
         }
     }
 }
---
+++ FF8ModTest/Archive/IndexEntryTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Xunit;
+using Sleepey.FF8Mod;
+using Sleepey.FF8Mod.Archive;
+
+namespace Sleepey.FF8ModTest.Archive
+{
+    public class IndexEntryTest
+    {
+        [Fact]
+        public void IndexEntryMinTest()
+        {
+            var entry = new IndexEntry(0, 0, 0);
+            Assert.Equal(0U, entry.Length);
+            Assert.Equal(0U, entry.Location);
+            Assert.Equal(0U, entry.Compression);
+        }
+
+        [Fact]
+        public void IndexEntryMaxTest()
+        {
+            var entry = new IndexEntry(uint.MaxValue, uint.MaxValue, 2);
+            Assert.Equal(uint.MaxValue, entry.Length);
+            Assert.Equal(uint.MaxValue, entry.Location);
+            Assert.Equal(2U, entry.Compression);
+        }
+
+        [Fact]
+        public void IndexEntryUnknownCompressionTest() => Assert.Throws<InvalidDataException>(() => new IndexEntry(0, 0, 3));
+
+        [Fact]
+        public void IndexEntryNullBytesTest() => Assert.Throws<ArgumentNullException>(() => new IndexEntry(null));
+
+        [Fact]
+        public void IndexEntryEmptyBytesTest() => Assert.Throws<InvalidDataException>(() => new IndexEntry(new byte[] { }));
+
+        [Fact]
+        public void IndexEntryShortBytesTest() => Assert.Throws<InvalidDataException>(() => new IndexEntry(new byte[11]));
+
+        [Fact]
+        public void IndexEntryLongBytesTest() => Assert.Throws<InvalidDataException>(() => new IndexEntry(new byte[13]));
+
+        [Fact]
+        public void IndexEntryMinBytesTest()
+        {
+            var entry = new IndexEntry(new byte[12]);
+            Assert.Equal(0U, entry.Length);
+            Assert.Equal(0U, entry.Location);
+            Assert.Equal(0U, entry.Compression);
+        }
+
+        [Fact]
+        public void IndexEntryMaxBytesTest()
+        {
+            var bytes = new byte[12];
+            for (int i = 0; i < 8; i++) bytes[i] = byte.MaxValue;
+            bytes[8] = 2;
+            var entry = new IndexEntry(bytes);
+            Assert.Equal(uint.MaxValue, entry.Length);
+            Assert.Equal(uint.MaxValue, entry.Location);
+            Assert.Equal(2U, entry.Compression);
+        }
+
+        [Fact]
+        public void IndexEntryUnknownCompressionBytesTest()
+        {
+            var bytes = new byte[12];
+            bytes[8] = 3;
+            Assert.Throws<InvalidDataException>(() => new IndexEntry(bytes));
+        }
+    }
+}
